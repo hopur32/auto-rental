986be17f2a735e52a5791251985c6fae424a0be9
Árni Dagur <arnidg@protonmail.ch>
2018-12-02 17:25:06 +0000

    Refactor and add functionality to table.py

--- table.py
+++ table.py
@@ -4,91 +4,27 @@ from collections import defaultdict
 from asciimatics.event import KeyboardEvent
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
-from asciimatics.widgets import MultiColumnListBox, Text, Frame, Layout, Widget, TextBox
-from asciimatics.exceptions import ResizeScreenError, StopApplication
+from asciimatics.widgets import MultiColumnListBox, Text, Frame, Layout, Widget, TextBox, Button
+from asciimatics.exceptions import ResizeScreenError, StopApplication, NextScene
+
+# import logging
+# logging.basicConfig(filename='/tmp/debug.log',level=logging.DEBUG)
 
 """
 Attributes:
     Public:
+        self.current_row
     Private:
-        self.__spacing
         self.__rows
         self.__num_cols
         self.__col_widths
         self.__col_names
-
-        self.__header
-        self.__list
-        self.__last_frame
 """
-class TableFrame(Frame):
-    def __init__(self, screen, rows, col_names, header_text='TableFrame', spacing=1, has_border=False):
+class Table():
+    def __init__(self, rows, col_names):
         self.__col_names = col_names
         self.set_rows(rows)
-        self.__spacing = spacing
-
-        # Asciimatics stuff:
-        # ----------------------------------------------------------------------
-        super().__init__(
-            screen, screen.height, screen.width, has_border=has_border, name=header_text
-        )
-        layout = Layout([1], fill_frame=True)
-        # Header
-        self.__header = TextBox(1, as_string=True)
-        self.__header.disabled = True
-        self.__header.custom_colour = "label"
-        self.__header.value = header_text
-        # List of rows
-        self.__list = MultiColumnListBox(
-            Widget.FILL_FRAME,
-            self.get_column_widths(spacing=self.__spacing),
-            [],
-            titles=self.__col_names,
-            name='Table'
-        )
-
-        self.add_layout(layout)
-        layout.add_widget(self.__header)
-        layout.add_widget(self.__list)
-        self.fix()
-
-        # Change colours
-        self.set_theme('monochrome')
-
-    def process_event(self, event):
-        # Do the key handling for this Frame.
-        if isinstance(event, KeyboardEvent):
-            if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
-                raise StopApplication("User quit")
-
-            # Force a refresh for improved responsiveness
-            self.__last_frame = 0
-
-        # Now pass on to lower levels for normal handling of the event.
-        return super().process_event(event)
-
-    def _update(self, frame_num):
-        if frame_num - self.__last_frame >= self.frame_update_count or self.__last_frame == 0:
-            self.__last_frame = frame_num
-
-            prev_value = self.__list.value
-            prev_start_line = self.__list.start_line
-
-            list_data = [(row, i+1) for i, row in enumerate(self.get_rows())]
-            column_widths = self.get_column_widths(spacing=self.__spacing)
-
-            self.__list.options = list_data
-            self.__list.value = prev_value
-            self.__list.start_line = prev_start_line
-            # Here we are editing a private attribute, and must thus be careful.
-            # This is not an intended usecase by the module authors.
-            self.__list._columns = column_widths
-
-        super()._update(frame_num)
-
-    @property
-    def frame_update_count(self):
-        return 40
+        self.current_row = None
 
     # Row management:
     # --------------------------------------------------------------------------
@@ -97,6 +33,15 @@ class TableFrame(Frame):
     """
     def get_rows(self):
         return self.__rows
+
+    def get_row(self, row_index):
+        return self.__rows[row_index]
+
+    def get_current_row(self):
+        if self.current_row == None:
+            return [''] * self.__num_cols
+        else:
+            return self.__rows[self.current_row]
 
     """
     Validates input and sets self.__rows, self.__num_cols,
@@ -117,10 +62,25 @@ class TableFrame(Frame):
 
         self.__rows = rows
         self.__num_cols = first_num_cols
-        self.__col_widths = self._init_column_widths()
+        self._init_column_widths()
 
-        # Force a refresh for improved responsiveness
-        self.__last_frame = 0
+    def set_row(self, row, row_index):
+        self.__rows[row_index] = row
+        self._update_column_widths([len(col) for col in row])
+
+    def edit_current_row(self, row):
+        if self.current_row == None:
+            self.add_row(row)
+        else:
+            self.set_row(row, self.current_row)
+
+    """
+    Delete the row at row_index. Does nothing if row_index == None.
+    """
+    def del_row(self, row_index):
+        if row_index != None:
+            del self.__rows[row_index]
+            self._init_column_widths()
 
     """
     Add a row to table.
@@ -140,14 +100,7 @@ class TableFrame(Frame):
             ))
 
         self.__rows.append(row)
-        self.__num_cols += 1
-
-        # Update self.__col_widths.
-        new_row_lengths = [len(col) for col in row]
-        self.__col_widths = [max(new, old) for new, old in zip(new_row_lengths, self.__col_widths)]
-
-        # Force a refresh for improved responsiveness
-        self.__last_frame = 0
+        self._update_column_widths([len(col) for col in row])
 
     # Column widths:
     # --------------------------------------------------------------------------
@@ -156,27 +109,180 @@ class TableFrame(Frame):
     largest cell in each column.
     """
     def _init_column_widths(self):
-        widths = []
         rows = self.__rows[:]
-        if self.__col_names:
-            rows.append(self.__col_names)
+        rows.append(self.__col_names)
+
+        widths = []
         for i in range(self.__num_cols):
             widths_in_col_i = [len(row[i]) for row in rows]
             biggest_width_in_col_i = max(widths_in_col_i)
             widths.append(biggest_width_in_col_i)
-        return widths
 
-    def get_column_widths(self, spacing=None):
-        if spacing:
-            return [col_width + spacing for col_width in self.__col_widths]
-        else:
+        self.__col_widths = widths
+
+    def _update_column_widths(self, new_row_widths):
+        self.__col_widths = [max(new, old) for new, old in zip(new_row_widths, self.__col_widths)]
+
+    def get_column_widths(self):
             return self.__col_widths
+
+    # Get other private attributes:
+    # --------------------------------------------------------------------------
+    def get_column_names(self):
+        return self.__col_names
+
+    def get_num_columns(self):
+        return self.__num_cols
+
+"""
+Attributes:
+    Public:
+        self.table
+    Private:
+        self.__spacing
+
+        self.__header
+        self.__list
+        self.__last_frame
+"""
+class TableFrame(Frame):
+    def __init__(self, screen, table, edit_scene, header_text='TableFrame', spacing=1, has_border=False):
+        self.table = table
+        self.__edit_scene = edit_scene
+        self.__spacing = spacing
+        super().__init__(
+            screen, screen.height, screen.width, has_border=has_border,
+            name=header_text, on_load=self._reload_list
+        )
+        layout = Layout([1], fill_frame=True)
+        # Header
+        self.__header = TextBox(1, as_string=True)
+        self.__header.disabled = True
+        self.__header.custom_colour = "label"
+        self.__header.value = header_text
+        # List of rows
+        self.__list = MultiColumnListBox(
+            Widget.FILL_FRAME,
+            [w + self.__spacing for w in self.table.get_column_widths()],
+            [],
+            titles=self.table.get_column_names(),
+            name='row_index'
+        )
+
+        self.add_layout(layout)
+        layout.add_widget(self.__header)
+        layout.add_widget(self.__list)
+        self.fix()
+
+        # Change colours
+        self.set_theme('monochrome')
+
+    def process_event(self, event):
+        # Do the key handling for this Frame.
+        if isinstance(event, KeyboardEvent):
+            if event.key_code in [ord('q'), ord('Q'), Screen.ctrl('c')]:
+                raise StopApplication("User quit")
+            elif event.key_code in [ord('a'), ord('A')]:
+                self._add()
+            elif event.key_code in [ord('e'), ord('E')]:
+                self._edit()
+            elif event.key_code in [ord('d'), ord('D')]:
+                self._delete()
+
+        # Now pass on to lower levels for normal handling of the event.
+        return super().process_event(event)
+
+    def _add(self):
+        self.table.current_row = None
+        raise NextScene(self.__edit_scene)
+
+    def _edit(self):
+        self.save()
+        self.table.current_row = self.data['row_index']
+        raise NextScene(self.__edit_scene)
+
+    """
+    Delete the selcted row from self.table and redraws the list.
+    """
+    def _delete(self):
+        self.save()
+        self.table.del_row(self.data['row_index'])
+        self._reload_list()
+
+    def _reload_list(self):
+        # prev_value = self.__list.value
+        # prev_start_line = self.__list.start_line
+
+        list_data = [(row, i) for i, row in enumerate(self.table.get_rows())]
+        column_widths = [w + self.__spacing for w in self.table.get_column_widths()]
+
+        self.__list.options = list_data
+        # self.__list.value = prev_value
+        # self.__list.start_line = prev_start_line
+        # Here we are editing a private attribute, and must thus be careful.
+        # This is not an intended usecase by the module authors.
+        self.__list._columns = column_widths
+
+class EditFrame(Frame):
+    def __init__(self, screen, table, table_scene, field_names):
+        self.table = table
+        self.__table_scene = table_scene
+        self.__field_names = field_names
+        self.__num_fields = len(field_names)
+
+        super().__init__(
+            screen, screen.height, screen.width,
+            hover_focus=True, can_scroll=True, reduce_cpu=True
+        )
+
+        main_layout = Layout([100], fill_frame=True)
+        self.add_layout(main_layout)
+        for field_name in self.__field_names:
+            main_layout.add_widget(Text(field_name, field_name))
+
+        bottom_layout = Layout([1, 1, 1, 1])
+        self.add_layout(bottom_layout)
+        bottom_layout.add_widget(Button("OK", self._ok), 0)
+        bottom_layout.add_widget(Button("Cancel", self._ok), 3)
+
+        self.fix()
+        self.set_theme('monochrome')
+
+    # TODO: Change Table() inner representation from list of lists to list of
+    #       dicts, so this is not needed anymore.
+    def _field_dict_from_row(self, row):
+        field_dict = {}
+        for field_name, field in zip(self.__field_names, row):
+            field_dict[field_name] = field
+        return field_dict
+
+    def reset(self):
+        # Do low-level reset
+        super().reset()
+        # Fill out fields from currently selected row
+        row = self.table.get_current_row()
+        self.data = self._field_dict_from_row(row)
+
+    def _ok(self):
+        self.save()
+        self.table.edit_current_row(list(self.data.values()))
+        raise NextScene(self.__table_scene)
+
+    @staticmethod
+    def _cancel():
+        raise NextScene(self.__table_scene)
+
+
 
 rows = [[str(i)+'dstndtns', str(i+1)+'12312312', str(i+2)+'tsdndtns']
         for i in range(0, 100, 3)]
 
+table = Table(rows, ['one', 'two', 'three'])
 def demo(screen):
-    screen.play([Scene([TableFrame(screen, rows, col_names=['one', 'two', 'three', 'four', 'five'])], -1)], stop_on_resize=True)
+    screen.play([
+        Scene([TableFrame(screen, table, 'edit')], -1, name='table'),
+        Scene([EditFrame(screen, table, 'table', table.get_column_names())], -1, name='edit')
+    ], stop_on_resize=True)
 
 while True:
     try:
