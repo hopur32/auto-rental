649a603f55d40ebee7ee07a38b411fdec2ed2570
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-12-04 16:21:27 +0000

    Implemented loading function

--- Data/Data.py
+++ Data/Data.py
@@ -1,18 +1,32 @@
-import csv
-
 class Data():
     'Takes a filename (without file extension) from the savedata folder as input'
     def __init__(self, path):
         self.path = path
+        self.__types, self.__col_names, self.rows = self.load()
     def load(self):
         try:
-            with open("savedata/" + self.path + ".csv", "r") as data:
-                pass
-        except:
-            return []
+            with open("savedata/" + self.path + ".txt", "r", encoding = "utf-8") as data:
+                types = data.readline().strip().split('|')
+                header = data.readline().strip().split('|')
+                rows = data.readlines()
+                for i in range(len(rows)):
+                    rows[i] = rows[i].strip().split('|')
+                return types, header, rows
+        except IndexError:
+            return [], [], [[]]
     def append(self, savedata):
-        with open("savedata/" + self.path + ".csv", "a+"):
+        with open("savedata/" + self.path + ".csv", "a+") as csvfile:
             pass
     def overwrite(self, savedata):
-         with open("savedata/" + self.path + ".csv", "w"):
-             pass
+        with open("savedata/" + self.path + ".csv", "w") as csvfile:
+            pass
+    def __str__(self):
+        s = " ".join(self.__types)
+        s += "\n" + " ".join(self.__col_names)
+        for i in self.rows:
+            s += "\n"
+            s += " ".join(i)
+        return s
+
+d = Data("Vehicles")
+print(d)
