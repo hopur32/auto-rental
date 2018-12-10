8ab60f4ac3b5533a7192580a0de6ddf8fbf91325
Eva <evag18@ru.is>
2018-12-10 14:57:41 +0000

    Added insurance to price list

--- Main.py
+++ Main.py
@@ -23,14 +23,27 @@ vehicletable = Table(
 #  orders = [['aeo123ao', 'Lamborgini', True, False]]
 #  ordertable = Table('ord', ['ID', 'Model', 'Is awesome', 'GPS'])
 
-PRICES = [
-    ['Small cars:', '2.900 ISK', '20.300 ISK', '87.000 ISK', '389.000 ISK'],
-    ['Medium cars:', '3.900 ISK', '27.300 ISK', '117.000 ISK', '489.000 ISK'],
-    ['Large cars:', '4.900 ISK', '34.300 ISK', '147.000 ISK', '598.000 ISK'],
-    ['Jeep:', '5.900 ISK', '41.300 ISK', '177.000 ISK', '998.000 ISK'],
-]
+# PRICES = [
+#     ['Small cars:', '2.900 ISK', '20.300 ISK', '87.000 ISK', '389.000 ISK'],
+#     ['Medium cars:', '3.900 ISK', '27.300 ISK', '117.000 ISK', '489.000 ISK'],
+#     ['Large cars:', '4.900 ISK', '34.300 ISK', '147.000 ISK', '598.000 ISK'],
+#     ['Jeep:', '5.900 ISK', '41.300 ISK', '177.000 ISK', '998.000 ISK'],
+# ]
+# PRICE_LIST= [
+# ['Small Car', '2900 ISK', '20300 ISK', '87000 ISK', '527800 ISK']
+# ['Medium car', '3900 ISK', '27300 ISK', '117000 ISK', '709800 ISK']
+# ['Large car', '4900 ISK', '34300 ISK', '147000 ISK', '891800 ISK']
+# ['Jeep', '5900 ISK', '41300 ISK', '177000 ISK', '1073800 ISK']
+# ['Basic insurance', '0 ISK', '0 ISK', '0 ISK', '0 ISK']
+# ['silver insurance', '250 ISK', '1750 ISK', '7500 ISK', '45500 ISK']
+# ['Gold insurance', '450 ISK', '3150 ISK', '13500 ISK', '81900 ISK'] ]
 
-#PRICES_TABLE = Table(PRICES, ['', '1 day', '1 week', '1 month', '6 months'])
+PRICE = {'Small Car':2900, 'Medium car': 3900, 'Large car': 4900, 'Jeep': 5900,
+'Basic insurance': 0, 'silver insurance': 250, 'Gold insurance': 450}
+PRICE_LIST= [[key, '{} ISK'.format(value ), '{} ISK'.format(value*7 ), '{} ISK'.format(value*30 ), 
+'{} ISK'.format(value*182)] for key, value in PRICE.items()]
+
+#PRICES_TABLE = Table(PRICE_LIST, ['', '1 day', '1 week', '1 month', '6 months'])
 
 
 def demo(screen):
