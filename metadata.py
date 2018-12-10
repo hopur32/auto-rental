c453520e7f522bc1e46e8a496357a46b3450c3a2
Árni Dagur <arnidg@protonmail.ch>
2018-12-10 12:09:20 +0000

    Create logging_dir if it doesn't exist on Windows

--- metadata.py
+++ metadata.py
@@ -10,11 +10,12 @@ AUTHOR = 'hopur-32'
 # Information for computers:
 # -----------------------------------------------------------------------------
 DATA_DIR = user_data_dir(APPNAME, AUTHOR) # OS specific directory to store data
+if not os.path.isdir(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
 if sys.platform == 'linux':
     LOGGING_DIR = '/tmp'
 else:
     LOGGING_DIR = user_log_dir(APPNAME, AUTHOR)
-
-
-if not os.path.isdir(DATA_DIR):
-    os.makedirs(DATA_DIR)
+    if not os.path.isdir(LOGGING_DIR):
+        os.makedirs(LOGGING_DIR)
