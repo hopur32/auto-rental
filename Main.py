354a65f0f1b643a0e0514075439d94e7fe54609b
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-12-10 14:29:07 +0000

    Added table for Vehicles and Orders, and expanded info for customer Table

--- Main.py
+++ Main.py
@@ -8,26 +8,28 @@ from asciimatics.exceptions import ResizeScreenError
 from datetime import datetime
 
 customers = [[3110002920, 'Árni', 'Dagur', True, datetime(2000, 10, 31)],
-             [1506995079, 'Viktor', 'Máni', False, datetime.now()]]
+             [1506992079, 'Viktor', 'Máni', False, datetime.now()]]
 customertable = Table(
     'Customers.txt',
-    ['Kennitala', 'First Name', 'Last name', 'Is awesome', 'DOB'],
-    [int, str, str, bool, datetime]
+    ['Kennitala', 'First Name', 'Last name', 'Phone Nr.', 'Email', 'DOB', 'Credit Card', 'Expiration Date', 'Ethnicity'],
+    [int, str, str, str, str, datetime, str, datetime, str]
 )
 vehicletable = Table(
     'Vehicles.txt',
-    ['Number of seats', 'Fuel consumption', 'Model', 'Year', 'Airbags'],
-    [int, float, str, datetime, bool]
+    ['License Plate', 'Manufacturer', 'Model', 'Year', 'Location', 'Category', 'Available'],
+    [str, str, str, int, str, str, bool]
 )
-
-#  orders = [['aeo123ao', 'Lamborgini', True, False]]
-#  ordertable = Table('ord', ['ID', 'Model', 'Is awesome', 'GPS'])
+ordertable = Table(
+    'Orders.txt',
+    ['ID', 'Customer', 'Vehicle', 'Start Date', 'End Date', 'Insurance', 'Extra Objects'],
+    [str, str, str, datetime, datetime, str, str]
+)
 
 PRICES = [
     ['Small cars:', '2.900 ISK', '20.300 ISK', '87.000 ISK', '389.000 ISK'],
     ['Medium cars:', '3.900 ISK', '27.300 ISK', '117.000 ISK', '489.000 ISK'],
     ['Large cars:', '4.900 ISK', '34.300 ISK', '147.000 ISK', '598.000 ISK'],
-    ['Jeep:', '5.900 ISK', '41.300 ISK', '177.000 ISK', '998.000 ISK'],
+    ['Jeeps:', '5.900 ISK', '41.300 ISK', '177.000 ISK', '998.000 ISK'],
 ]
 
 #PRICES_TABLE = Table(PRICES, ['', '1 day', '1 week', '1 month', '6 months'])
@@ -36,9 +38,9 @@ PRICES = [
 def demo(screen):
     screen.play([
         #Scene([TableFrame(screen, PRICES_TABLE, 'pricelist', 'Prices')], -1, name='pricelist'),
-        Scene([TableFrame(screen, vehicletable, 'customeredit', 'Customers')], -
+        Scene([TableFrame(screen, ordertable, 'customeredit', 'Customers')], -
               1, name='customertable'),
-        Scene([EditFrame(screen, vehicletable, 'customertable')], -
+        Scene([EditFrame(screen, ordertable, 'customertable')], -
               1, name='customeredit'),
         #  Scene([TableFrame(screen, ordertable, 'orderedit')], -1, name='ordertable'),
         #  Scene([EditFrame(screen, ordertable, 'ordertable')], -1, name='orderedit')
