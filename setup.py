5510349fbdde3ec160ff6475b504f7edd27b21b5
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-12-13 23:20:33 +0000

    Changed 'Setup.py' after learning a bit more about how it works

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 # 'python setup.py install' to install required modules
-setup(name='asciimatics',
-      version='1.10.0',
-      )
-setup(name='appdirs',
-      version='1.4.3',
+setup(name='Auto-Rental',
+      version='1.0',
+      author='Hópur 32',
+      url='https://github.com/vln-1-2018-hopur-32/auto-rental',
+      description="School project emulating a car rental services' program",
+      requires=['asciimatics', 'appdirs',],
       )
