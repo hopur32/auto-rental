d870c60e78610969bdf63d2152ddc2edc08c5990
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-12-14 00:00:58 +0000

    Gained competence

--- gitCommit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git add .
-if [ $# -lt 1 ]
-then
-	git commit -m "Auto-commit"
-else
-	git commit -m "$1"
-fi
-git push -u origin master
--- gitPull.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git pull https://github.com/vln-1-2018-hopur-32/auto-rental.git 
